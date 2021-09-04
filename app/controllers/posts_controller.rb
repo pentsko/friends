c15bbc238dfd8a12166b833473bbb9c8629b8fd7
Taras <pentsko.t@gmail.com>
2021-09-04 09:10:20 +0300

    working all CRUD work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,24 @@
 class PostsController < ApplicationController
+	before_action :authenticate_user!, except: [:index]
+
+	def edit
+		@post = Post.find_by id: params[:id]
+	end
+
+	def update
+		@post = Post.find_by id: params[:id]
+		if @post.update post_params
+			redirect_to posts_path
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@post = Post.find_by id: params[:id]
+		@post.destroy
+		redirect_to posts_path
+	end
 
 	def index
 		@posts = Post.all
@@ -9,7 +29,20 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		render plain: params
+		@post = Post.new post_params
+		if @post.save
+			redirect_to posts_path
+		else
+			render :new
+		end
 	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :body, :author)
+	end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,13 @@ Rails.application.routes.draw do
   resources :posts
   get 'home/about'
   get 'home/contacts'
-  get 'home/posts', to: 'posts#index'
-  get 'home/posts/new', to: 'posts#new'
+  get '/posts', to: 'posts#index'
+  get '/posts/new', to: 'posts#new'
+  get '/posts/:id/edit', to: 'posts#edit'
+  get '/posts/id', to: 'posts#update'
+  get '/posts/id', to: 'posts#destroy'
   post '/posts', to: 'posts#create'
+
 
   # get 'home/index'
   root 'friends#index'
