79a0ef64d9d9111075423b4a3adc3f1b2eec9a30
Taras <pentsko.t@gmail.com>
2021-09-10 07:53:47 +0300

    show all posts in main page, but can't do edit without authorization

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
 
   # get 'home/index'
-  root 'friends#index'
+  root 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
