68d555e08278906ef797ddd0bbfc308029da7483
Taras <pentsko.t@gmail.com>
2021-09-18 01:19:46 +0300

    add column NAME to users table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-	validates :title, presence: true, length: {minimum: 3}
+	validates :title, presence: true, length: {minimum: 1}
 	validates :body, presence: true, length: {minimum: 2}
 	validates :author, presence: true, length: {minimum: 2}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20210917213203) do
 
-ActiveRecord::Schema.define(version: 20210910183007) do
   create_table "connections", force: :cascade do |t|
     t.integer "user_id"
     t.integer "friend_id"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20210910183007) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
