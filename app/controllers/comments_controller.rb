00093d1f21ab566f25761dcfb878504d823c4c47
Taras <pentsko.t@gmail.com>
2021-11-02 07:09:54 +0200

    add footer, but need to reload page twice every time, and need to work with styles of footer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
 
   def create
       @comment = current_user.comments.new(comment_params)
+
     if !@comment.save
       flash[:notice] = @comment.errors.full_messages.to_sentence
     end
