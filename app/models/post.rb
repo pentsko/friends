56cce3919f172a4cdc614651b75b82456067d522
Taras <pentsko.t@gmail.com>
2021-11-14 19:44:57 +0200

    refactor author name in postsa

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 1 }
   validates :body, presence: true, length: { minimum: 2 }
-  validates :author, presence: true, length: { minimum: 2 }
   belongs_to :user
   has_many :comments
   has_many :likes
