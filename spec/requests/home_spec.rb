e67691e70e56ff7dd8eaed3f87b14d63e827e4dd
Taras <pentsko.t@gmail.com>
2021-12-08 07:16:23 +0200

    add test to home requests

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -8,6 +8,16 @@ RSpec.describe "Homes", type: :request do
         get root_path
         expect(response).to render_template("home/index")
       end
+
+      it "show contact page" do
+        get home_contacts_path
+        expect(response.status).to eq(200)
+      end
+
+      it "show about page" do
+        get home_about_path
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 end
